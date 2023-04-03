fc848e5ac7b485593ebdfdc6e752f46ef23c6d47
Julien Goolsby <juliengoolsby@gmail.com>
2023-04-03 14:37:03 -0500

    initial commit

---
+++ src/Global.js
@@ -0,0 +1,9 @@
+var patientReport;
+
+function setPatientReport(aPatientReport){
+    patientReport = aPatientReport;
+}
+
+function getPatientReport(){
+    return patientReport;
+}
---
+++ src/classes/PatientReport.js
@@ -0,0 +1,14 @@
+export class PatientReport {
+    constructor(firstname, lastname, sex, birthday, injury) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.sex = sex;
+        this.birthday = birthday;
+        this.injury = injury;
+
+    }
+
+
+} function getAssignedDoctor(injury) {
+    return "Dr. " + "Jerry";
+}
